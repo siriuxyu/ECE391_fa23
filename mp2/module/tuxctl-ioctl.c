19f13d542a4778482baa1305bb42e9139597326d
Siriuxxx <hengjia.y@outlook.com>
2023-10-07 18:41:21 -0500

    tux mp2.2

--- mp2/module/tuxctl-ioctl.c
+++ mp2/module/tuxctl-ioctl.c
@@ -38,10 +38,9 @@ static unsigned char LED_pattern[4];
 static unsigned char buttons;
 static unsigned long EFLAGS;
 
-
+// 7-segment display number table
 static char number_table[16] {
 	0xE7, 			// 0
-	0x06, 			// 1
 	0xCB, 			// 2
 	0x8F, 			// 3
 	0x2E, 			// 4
@@ -90,11 +89,31 @@ void tuxctl_handle_packet (struct tty_struct* tty, unsigned char* packet)
     /*printk("packet : %x %x %x\n", a, b, c); */
 }
 
+
+/*
+ * tuxctl_ack()
+ *   DESCRIPTION: Acknowledge the tux controller
+ *   INPUTS: none
+ *   OUTPUTS: none
+ *   RETURN VALUE: none
+ *   SIDE EFFECTS: set ack_flag to 1
+*/
+
 void tuxctl_ack()
 {
 	ack_flag = 1;
 }
 
+
+/*
+ * tuxctl_bioc_event()
+ *   DESCRIPTION: Handle the button event
+ *   INPUTS: arg1 -- the first byte of the packet
+ *			 arg2 -- the second byte of the packet
+ *   OUTPUTS: none
+ *   RETURN VALUE: none
+ *   SIDE EFFECTS: set buttons to the button status
+*/
 
 void tuxctl_bioc_event(unsigned arg1, unsigned arg2)
 {
@@ -102,7 +121,7 @@ void tuxctl_bioc_event(unsigned arg1, unsigned arg2)
 
 	buttons = 0;
 	buttons |= arg1 & 0x0F;
-	buttons |= (arg2 & 0x01) << 4;
+	buttons |= (arg2 & 0x01) << 4;		// right | left | down | up
 	buttons |= (arg2 & 0x02) << 5;
 	buttons |= (arg2 & 0x04) << 3;
 	buttons |= (arg2 & 0x08) << 4;
@@ -111,9 +130,19 @@ void tuxctl_bioc_event(unsigned arg1, unsigned arg2)
 }
 
 
+/*
+ * tuxctl_reset()
+ *   DESCRIPTION: Reset the tux controller
+ *   INPUTS: arg1 -- the first byte of the packet
+ *			 arg2 -- the second byte of the packet
+ *   OUTPUTS: none
+ *   RETURN VALUE: none
+ *   SIDE EFFECTS: reset the tux controller
+*/
+
 void tuxctl_reset(tty_struct* tty)
 {
-	unsigned char packet[8];
+	unsigned char packet[8];				// 8 bytes of the packet
 
 	packet[0] = MTCP_BIOC_ON;
 	packet[1] = MTCP_LED_USR;
@@ -123,7 +152,7 @@ void tuxctl_reset(tty_struct* tty)
 	int idx;
 	for (idx = 4; idx < 8; idx++) {
 		packet[idx] = 0x00;
-		LED_pattern[idx - 4] = 0x00;
+		LED_pattern[idx - 4] = 0x00;		// reset the LED to 0
 	}
 
 	if (ack_flag) {
@@ -152,6 +181,14 @@ void tuxctl_reset(tty_struct* tty)
  *                                                                            *
  ******************************************************************************/
 
+/*
+ * tuxctl_init()
+ *   DESCRIPTION: Initialize the tux controller
+ *   INPUTS: tty -- the tty_struct of the tux controller
+ *   OUTPUTS: none
+ *   RETURN VALUE: 0
+ *   SIDE EFFECTS: initialize the tux controller
+*/
 int32_t process_button() {
 	int32_t button = 0;
 	spin_lock_irqsave(&lock, EFLAGS);
@@ -160,6 +197,15 @@ int32_t process_button() {
 	return button;
 }
 
+
+/*
+ * tuxctl_init()
+ *   DESCRIPTION: Initialize the tux controller
+ *   INPUTS: tty -- the tty_struct of the tux controller
+ *   OUTPUTS: none
+ *   RETURN VALUE: 0
+ *   SIDE EFFECTS: initialize the tux controller
+*/
 int tuxctl_init(struct tty_struct* tty)
 {
 	if (!ack_flag) {
@@ -179,7 +225,14 @@ int tuxctl_init(struct tty_struct* tty)
 	return 0;
 }
 
-
+/*
+ * tuxctl_buttons()
+ *   DESCRIPTION: Get the button status
+ *   INPUTS: ptr -- the pointer to the button status
+ *   OUTPUTS: none
+ *   RETURN VALUE: 0 if success, -EINVAL if ptr is NULL
+ *   SIDE EFFECTS: none
+*/
 int tuxctl_buttons(int32_t* ptr)
 {
 	if (ptr == NULL){
@@ -193,7 +246,15 @@ int tuxctl_buttons(int32_t* ptr)
 	return 0;
 }
 
-
+/*
+ * tuxctl_set_LED()
+ *   DESCRIPTION: Set the LED pattern
+ *   INPUTS: tty -- the tty_struct of the tux controller
+ *			 arg -- the LED pattern
+ *   OUTPUTS: none
+ *   RETURN VALUE: 0 if success, -EINVAL if arg is invalid
+ *   SIDE EFFECTS: set the LED pattern
+*/
 int tuxctl_set_LED(struct tty_struct* tty, int32_t arg)
 {
 	if (arg < 0 || arg > 0xFFFFFFFF){
@@ -228,7 +289,17 @@ int tuxctl_set_LED(struct tty_struct* tty, int32_t arg)
 }
 
 
-
+/*
+ * tuxctl_ioctl()
+ *   DESCRIPTION: The ioctl function of the tux controller
+ *   INPUTS: tty -- the tty_struct of the tux controller
+ *			 file -- the file struct of the tux controller
+ *			 cmd -- the command
+ *			 arg -- the argument
+ *   OUTPUTS: none
+ *   RETURN VALUE: 0 if success, -EINVAL if cmd is invalid
+ *   SIDE EFFECTS: none
+*/
 int 
 tuxctl_ioctl (struct tty_struct* tty, struct file* file, 
 	      unsigned cmd, unsigned long arg)
